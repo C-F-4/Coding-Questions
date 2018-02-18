2067e544d82155514c692e07f608db4687639484
cfx.404 <C-F-4@users.noreply.github.com>
2018-02-18 15:07:35 +0530

    Update Triple-Pepsi.c

--- C/Triple-Pepsi.c
+++ C/Triple-Pepsi.c
@@ -10,7 +10,7 @@ int main(void) {
     int iter_x = 0;
     int available_drinks = 0;
     scanf("%d", &available_drinks);
-    int last_drink = -1;
+    int last_drink = ' ';
     while(1) {
         for (iter_x = 0; iter_x < CHARACTER_COUNT; iter_x++) {
             int clone_count = clone_counter[iter_x];
@@ -18,14 +18,37 @@ int main(void) {
             int generated_clones = clone_count * clone_multiplier;
             clone_counter[iter_x] = generated_clones;
             if (drink_counter >= available_drinks) {
-                last_drink = iter_x;
+                last_drink = character_array[iter_x];
                 break;
             }
         }
-        if (last_drink != -1) {
+        if (last_drink != ' ') {
             break;
         }
     }
-    printf("%d", last_drink);
+    printf("%c: ", last_drink);
+    switch(last_drink) {
+    case '0':
+        printf("Giri");
+        break;
+    case '1':
+        printf("Perichi");
+        break;
+    case '2':
+        printf("Collin");
+        break;
+    case '3':
+        printf("Clinton");
+        break;
+    case '4':
+        printf("Karthi");
+        break;
+    case '5':
+        printf("Jeeva");
+        break;
+    default:
+        printf("Something broke");
+        break;
+    }
     return 0;
 }
